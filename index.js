1e7186613c21c2c7a857394d4a9ebc3bad4c850f
Collins Kiragu <collins.kiragu@student.moringaschool.com>
2022-11-24 00:04:29 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = (num1=2000, num2=100) =>{
+    console.log("number 1 = " + num1);
+    console.log("number 1 = " + num2);
+    return num1 / num2;
+
+}
+let answer = divide(2000, 100)
+
+const square = x => x ** 2;
+square(2);
+
+const add = (a, b) => a + b;
+add(3,4);
